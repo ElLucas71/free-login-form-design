bde83c58a220e8a5b1357de73fd56b6e76529d16
Lucas <126828696+ElLucas71@users.noreply.github.com>
2023-03-08 19:53:13 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,65 @@
-    var email = document.forms['form']['email'];
-    var password = document.forms['form']['password'];
-    
-    var email_error = document.getElementById('email_error');
-    var pass_error = document.getElementById('pass_error');
-    
-    email.addEventListener('textInput', email_Verify);
-    password.addEventListener('textInput', pass_Verify);
-    
-    function validated(){
-        if (email.value.length < 9) {
-            email.style.border = "1px solid red";
-            email_error.style.display = "block";
-            email.focus();
-            return false;
-        }
-        if (password.value.length < 6) {
-            password.style.border = "1px solid red";
-            pass_error.style.display = "block";
-            password.focus();
-            return false;
-        }
-    
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+var repeatpassword = document.forms['form']['repeatpassword']
+var repeatpassword = document.forms['form']['username']
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+var pass_error2 = document.getElementById('pass_error2');
+var username_error = document.getElementById('pass_error2');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+repeatpassword.addEventListener('textInput', pass_Verify2);
+username.addEventListener('textInput', username_verify);
+
+function validated(){
+    if (email.value.length < 9) {
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
     }
-    function email_Verify(){
-        if (email.value.length >= 8) {
-            email.style.border = "1px solid silver";
-            email_error.style.display = "none";
-            return true;
-        }
+    if (password.value.length < 6) {
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
     }
-    function pass_Verify(){
-        if (password.value.length >= 6) {
-            password.style.border = "1px solid silver";
-            pass_error.style.display = "none";
-            return true;
-        }
+    if (password.value != repeatpassword.value) {
+        repeatpassword.style.border = "1px solid red";
+        pass_error2.style.display = "block";
+        repeatpassword.focus();
+        return false;
     }
-    
-    
+
+}
+function email_Verify(){
+    if (email.value.length >= 8) {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+}
+function pass_Verify(){
+    if (password.value.length >= 6) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
+}
+function pass_Verify2(){
+    if (password.value != repeatpassword.value) {
+        repeatpassword.style.border = "1px solid silver";
+        pass_error2.style.display = "none";
+        return true;
+    }
+}
+
+function username_verify() {
+    if (username.value.length >= 1) {
+        username.style.border = "1px solid silver";
+        username_error.style.display = "none";
+        return true;
+    }
+}
