b8fae9276b0d3a266a53b15e4caa3df0f41dadb4
Lucas <126828696+ElLucas71@users.noreply.github.com>
2023-03-08 20:08:21 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,30 @@
 var email = document.forms['form']['email'];
 var password = document.forms['form']['password'];
 var repeatpassword = document.forms['form']['repeatpassword']
-var repeatpassword = document.forms['form']['username']
+var username = document.forms['form']['username']
 
 var email_error = document.getElementById('email_error');
 var pass_error = document.getElementById('pass_error');
 var pass_error2 = document.getElementById('pass_error2');
-var username_error = document.getElementById('pass_error2');
-
+var username_error = document.getElementById('username_error');
 email.addEventListener('textInput', email_Verify);
 password.addEventListener('textInput', pass_Verify);
 repeatpassword.addEventListener('textInput', pass_Verify2);
 username.addEventListener('textInput', username_verify);
 
 function validated(){
+    if (username.value.length < 4) {
+        username.style.border = "1px solid red";
+        username_error.style.display = "block";
+        username.focus();
+        return false;
+    }
+    if (username.value.length < 4) {
+        username.style.border = "1px solid red";
+        username_error.style.display = "block";
+        username.focus();
+        return false;
+    }
     if (email.value.length < 9) {
         email.style.border = "1px solid red";
         email_error.style.display = "block";
@@ -32,7 +43,6 @@ function validated(){
         repeatpassword.focus();
         return false;
     }
-
 }
 function email_Verify(){
     if (email.value.length >= 8) {
@@ -57,7 +67,7 @@ function pass_Verify2(){
 }
 
 function username_verify() {
-    if (username.value.length >= 1) {
+    if (username.value.length >= 4) {
         username.style.border = "1px solid silver";
         username_error.style.display = "none";
         return true;
